ae765e101e3c9f1ff0fcda46da990cd0536c27c4
Evan <e.c.kalaw@gmail.com>
2025-05-22 19:58:08 -0500

    First pass on some UI stuff (#3)

---
+++ app/components/content/MainContent.tsx
@@ -0,0 +1,14 @@
+export default function MainContent() {
+  return (
+    <div className="flex-col h-full text-3xl space-y-6">
+      <div>
+        I&apos;m a{" "}
+        <span className="text-[#5582ff] font-bold">
+          Full Stack Software Engineer
+        </span>{" "}
+        with a passion for building scalable and performant web applications
+        with great user experiences.
+      </div>
+    </div>
+  );
+}
