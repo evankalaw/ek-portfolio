5c46a50daeaeccdbdc5acf3add4db9bf484344a4
Evan Kalaw <e.c.kalaw@gmail.com>
2025-05-23 20:30:52 -0500

    ok

--- app/components/content/MainContent.tsx
+++ app/components/content/MainContent.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import WorkExperienceSection from "./WorkExperienceSection";
 
 export default function MainContent() {
@@ -6,19 +7,35 @@ export default function MainContent() {
       <div className="text-xl space-y-6">
         <div>
           I&apos;m a{" "}
-          <span className="text-[#5582ff] font-bold">
-            Full Stack Software Engineer
-          </span>{" "}
-          with a passion for building scalable and performant web applications
-          with great user experiences.
+          <span className="font-bold">Full Stack Software Engineer</span> with a
+          passion for building scalable and performant web applications with
+          great user experiences.
         </div>
         <div>
-          Most recently, I consulted with Eliza Studios, a creative studio,
-          where I built a comprehensive front end experience for their users
-          with Next.JS, Tailwind CSS, TypeScript, and Sanity CMS.
+          Most recently, I consulted with{" "}
+          <Link
+            href="https://elizastudios.ai"
+            target="_blank"
+            className="text-[#5582ff] font-bold underline"
+          >
+            Eliza Studios
+          </Link>
+          , a creative studio, where I built a comprehensive front end
+          experience for their users with Next.JS, Tailwind CSS, TypeScript, and
+          Sanity CMS.
         </div>
       </div>
-      <WorkExperienceSection />
+      <div>
+        <div className="text-xl font-bold">Education</div>
+        <div className="py-3">
+          <div>B.S. in Computer Science, University of Wisconsin-Madison</div>
+          <div>2015 - 2019</div>
+        </div>
+      </div>
+      <div>
+        <div className="text-xl font-bold">Work</div>
+        <WorkExperienceSection />
+      </div>
     </div>
   );
 }
