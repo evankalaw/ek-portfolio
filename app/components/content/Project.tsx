fef1a0230b8bfeae70e022a60e0000db91057791
Evan <e.c.kalaw@gmail.com>
2025-05-27 12:30:56 -0500

    Open project state

--- app/components/content/Project.tsx
+++ app/components/content/Project.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface Project {
   title: string;
   description: string;
@@ -5,10 +12,24 @@ interface Project {
 
 export default function Project(props: Project) {
   const { title, description } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="py-3">
-      <div className="font-bold">{title}</div>
-      <div>{description}</div>
+    <div className="p-3 hover:bg-gray-100 rounded-md">
+      <div className="flex justify-between items-center">
+        <div className="font-bold">{title}</div>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="cursor-pointer text-sm text-gray-500 border border-gray-300 rounded-md px-2 py-1"
+        >
+          {isOpen ? (
+            <FontAwesomeIcon icon={faChevronUp} />
+          ) : (
+            <FontAwesomeIcon icon={faChevronDown} />
+          )}
+        </button>
+      </div>
+      <div className={`${isOpen ? "block" : "hidden"} mt-2`}>{description}</div>
     </div>
   );
 }
